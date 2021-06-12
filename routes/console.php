faf9e116a8346034c9bee1efca0bcfd74be62aff
parables <parables95@gmail.com>
2021-06-12 23:36:21 +0000

    FEATURE: Added Passport Auth

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('create-admin', function () {
+    User::create([
+        'name' => 'Admin',
+        'email' => 'admin@admin.com',
+        'password' => bcrypt('adminadmin')
+    ]);
+})->describe('Create the admin');
